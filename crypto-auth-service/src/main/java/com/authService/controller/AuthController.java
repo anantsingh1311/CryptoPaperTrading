a46132ba0dc8a1d143acf173cba60b4901453b89
anantsingh1311 <62670882+anantsingh1311@users.noreply.github.com>
2026-06-15 13:40:23 +0530

    Use environment-driven auth CORS

--- crypto-auth-service/src/main/java/com/authService/controller/AuthController.java
+++ crypto-auth-service/src/main/java/com/authService/controller/AuthController.java
@@ -1,7 +1,6 @@
 package com.authService.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +12,6 @@ import com.authService.dto.RegisterRequest;
 import com.authService.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import com.authService.dto.LoginRequest;
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/auth")
 public class AuthController{
